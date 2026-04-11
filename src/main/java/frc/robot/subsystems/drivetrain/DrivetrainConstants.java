e011f6838db4e67d3e9c55d3b3529b83280112a5
mastermatika <maxlisien@gmail.com>
2026-04-11 10:32:06 -0400

    WIP: new module offsets

--- src/main/java/frc/robot/subsystems/drivetrain/DrivetrainConstants.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DrivetrainConstants.java
@@ -236,6 +236,6 @@ public class DrivetrainConstants {
 
     /** FL, FR, BL, BR. Negative rotation of each absolute encoder when the wheels face forward */
     public static final int[] zeroRotations_ticks =
-        new int[] {-2266 + 2048, -1452 + 2048, -3714 + 2048, -2407 + 2048};
+        new int[] {1837 + 2048 - 2157, 2048 - 1529 - 1994, 2273 + 2048 - 3930, 1339 + 2048 - 1751};
   }
 }
