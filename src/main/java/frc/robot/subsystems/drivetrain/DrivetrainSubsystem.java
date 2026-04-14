202195deace717e2113a25c321c426b9168837ac
8d6Equals28 <albertjkzhang@gmail.com>
2026-04-14 18:40:54 -0400

    Slight pathplanner tuning, still needs to be improved.

--- src/main/java/frc/robot/subsystems/drivetrain/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DrivetrainSubsystem.java
@@ -115,7 +115,8 @@ public class DrivetrainSubsystem extends SubsystemBase {
         this::getChassisSpeeds,
         this::setGoalVelocity,
         new PPHolonomicDriveController(
-            new PIDConstants(5.0, 0.0, 0.4), new PIDConstants(6, 0, 0.55)),
+            new PIDConstants(4.5, 0.0, 0.4),
+            new PIDConstants(5, 0, 0.55)), // 5.0, 0.0, 0.4, 6, 0, 0.55
         Chassis.ppConfig,
         () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red,
         // () -> {
