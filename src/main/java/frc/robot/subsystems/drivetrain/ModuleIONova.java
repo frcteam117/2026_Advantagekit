406ea3bc379c7aed67dc6ed8fa60c63f8b12feb4
blue-or-pink <jane.carol.berry@gmail.com>
2026-06-07 16:33:10 -0400

    WIP: changed to FL & BR are sparkmaxes, TODO still fix the other issues

--- src/main/java/frc/robot/subsystems/drivetrain/ModuleIONova.java
+++ src/main/java/frc/robot/subsystems/drivetrain/ModuleIONova.java
@@ -54,7 +54,7 @@ public class ModuleIONova implements ModuleIO {
   public ModuleIONova(int module) {
     moduleIndex = module;
     azimuthNova = new ThriftyNova(Azimuth.canIds[module], MotorType.NEO);
-    if (module != 3) {
+    if ((module == 1) || (module == 2)) { // FR, BL are still novas
       driveNova = new ThriftyNova(Drive.canIds[module], MotorType.NEO);
       driveSparkMax = null;
     } else {
